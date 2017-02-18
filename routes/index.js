3c9aa8fa5833059354f7eb9e8ee36414a71efcf7
Karthik <karthik.rp@hotmail.com>
2017-02-18 20:27:51 +0530

    Added Patrick Star Quotes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bender = require('../data/bender.json');
+var patrick = require('../data/patrick.json');
 
 function getRandom(maxNum) {
     return Math.floor(Math.random() * maxNum);
@@ -11,7 +12,11 @@ router.get('/', function (req, res) {
 });
 
 router.get("/bender", function (req, res) {
-    return res.send({ "Quote": bender.quotes[getRandom(bender.quotes.length)], "Author": "Bender Rodriguez" });
+    return res.send({ "Quote": bender.quotes[getRandom(bender.quotes.length)], "Author": bender.Author });
+});
+
+router.get("/patrick", function (req, res) {
+    return res.send({ "Quote": patrick.quotes[getRandom(patrick.quotes.length)], "Author": patrick.Author });
 });
 
 module.exports = router;
