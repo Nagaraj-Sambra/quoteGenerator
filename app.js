b91badb206d4e010ecf88383454349537c9b0b5a
Karthik RP <karthik.rp@hotmail.com>
2017-05-06 22:42:57 +0530

    Added openshift settings

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var router = require('./routes')
 app.use('/api', router);
 
-var port = process.env.PORT || 3000;
-var server = app.listen(port, function () {
+var server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.PORT || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server = app.listen(server_port, server_ip_address, function () {
     console.log("Listening on port %s...", server.address().port);
 });
