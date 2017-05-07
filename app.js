d7fb1a9757edbddf29cee56cdb7928366edda994
Karthik RP <karthik.rp@hotmail.com>
2017-05-07 19:30:36 +0530

    Added cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var cors = require('cors');
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 var router = require('./routes')
 app.use('/api', router);
